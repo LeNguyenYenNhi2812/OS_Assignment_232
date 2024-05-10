5497e72904011d532307d0244f80e20a034f1033
ynhi <vboxuser@linux.myguest.virtualbox.org>
2024-05-10 15:01:29 +0700

    comment and update scheduling

--- src/queue.c
+++ src/queue.c
@@ -2,65 +2,60 @@
 #include <stdlib.h>
 #include "queue.h"
 
-static int cur_prior = 272;
-int empty(struct queue_t * q) {
+int empty(struct queue_t * q) 
+{
         if (q == NULL) return 1;
 	return (q->size == 0);
 }
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
-	if(q[0].size == MAX_QUEUE_SIZE) return;
-	for(int i = MAX_QUEUE_SIZE - 1; i >= 0; i--) {
-		if(q[0].proc[i] == NULL) {
-			q[0].proc[i] = proc;
-			q[0].size++;
-			//printf("\nPID: %d\nSize: %d\n",q[0].proc[i]->pid, q[0].size); 
-			break;
-		}
-	} 
+        //if queue is null => cant add new pro
+        if (q == NULL)
+        {
+                perror("Queue is NULL !\n");
+                exit(1);
+        }
+        // if queue is full => cant add pro
+        if (q->size == MAX_QUEUE_SIZE)
+        {
+                perror("Queue is full !\n");
+                exit(1);
+        }
+        //add pro at the end of queue and
+        q->proc[q->size] = proc;
+        q->size++;
 }
-
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-       /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	// Pre-process (Assign cur_prior) (272 is default value when it's initialized
-	if (cur_prior == 272) {
-		for(int i = 0; i < MAX_PRIO; i++) 
-			if(q[i].size != 0) cur_prior = i;
-	}
-	cur_prior = 0;
-	// In-process 
-	// Case: cur priority-queue have 0 slot (1)
-	int countPriorQ = 0;
-	while(q[cur_prior].size == 0) {
-		if(countPriorQ >= MAX_PRIO) {
-			return NULL; // No more process
-		}
-		else {
-			cur_prior = (cur_prior + 1) % 140;
-			countPriorQ++;
-		}
-	} // Stop at prior-queue have process 
-	
-	// Take oldest process in cur prior-queue (2)
-	struct pcb_t *run = q[cur_prior].proc[MAX_QUEUE_SIZE - 1];// Take oldest process
-	q[cur_prior].proc[MAX_QUEUE_SIZE - 1] = q[cur_prior].proc[MAX_QUEUE_SIZE - 2];
-	// Shift-right all process in array (Just shift size - 1 elements) (3)
-	for(int i = MAX_QUEUE_SIZE - 2; i >= 1; i--) {
-		if(q[cur_prior].proc[i] == NULL) break;	// have shifted all process 
-		else {
-			q[cur_prior].proc[i] = q[cur_prior].proc[i - 1]; 
-		}
-	}
-
-	q[cur_prior].size--;
-	run->prio = (run->prio + 1 < 139) ? (run->prio + 1) : 139;
-	return run;
+         * in the queue [q] and remember to remove it from q */
+        if (q == NULL || q->size == 0) return NULL;
+        // if queue is null or empty return
+        struct pcb_t *temp = q->proc[0];
+        //temp is the first pro in queue
+#ifdef MLQ_SCHED
+        // move each process in queue then remove the last process
+        int length = q->size - 1; 
+        for (int i = 0; i < length; ++i) q->proc[i] = q->proc[i + 1];
+        q->proc[length] = NULL;
+        q->size--;
+        return temp;
+#else
+//remove process with the largest priority
+        int index = 0;
+        int length = q->size;
+        for (int i = 1; i < length; ++i)
+        {
+                if (temp->priority < q->proc[i]->priority)
+                {
+                        temp = q->proc[i];
+                        index = i;
+                }
+        }
+        q->proc[index] = q->proc[length - 1];
+        q->proc[length - 1] = NULL;
+        q->size--;
+        return temp;
+#endif
 }
-
